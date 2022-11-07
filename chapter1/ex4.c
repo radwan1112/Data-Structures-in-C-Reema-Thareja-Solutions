ae1efa6caaf1f31d18a260d5ada1f4a909a9d4c4
Radwan AlShawesh <radwan.al.shawesh@gmail.com>
2022-11-07 18:13:06 +0300

    new files

---
+++ chapter1/ex4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    char alphabit;
+    printf("Enter a character: ");
+    scanf("%c", &alphabit);
+    
+    switch (alphabit)
+    {
+    case '1':
+        break;
+    
+    default:
+        break;
+    }
+
+
+    return 0;
+}
