b7265b81968dae1c060bf837a41c31c23fc6b1a5
Radwan AlShawesh <radwan.al.shawesh@gmail.com>
2022-11-07 18:39:12 +0300

    finished ex4

--- chapter1/ex4.c
+++ chapter1/ex4.c
@@ -4,16 +4,32 @@ int main(){
     char alphabit;
     printf("Enter a character: ");
     scanf("%c", &alphabit);
-    
-    switch (alphabit)
-    {
-    case '1':
-        break;
-    
-    default:
-        break;
-    }
 
+    switch(alphabit)
+    {
+        case 'A':
+        case 'a':
+            printf("%c is a vowel", alphabit);
+            break;
+        case 'O':
+        case 'o':
+            printf("%c is a vowel", alphabit);
+            break;
+        case 'I':
+        case 'i':
+            printf("%c is a vowel", alphabit);
+            break;
+        case 'E':
+        case 'e':
+            printf("%c is a vowel", alphabit);
+            break;
+        case 'U':
+        case 'u':
+            printf("%c is a vowel", alphabit);
+            break;
+        default:
+            printf("%c is not  a vowel", alphabit);
+    }
 
     return 0;
 }
