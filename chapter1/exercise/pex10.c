146b6280f1ba569eaef487b1e68701f714dfac1e
Radwan AlShawesh <radwan.al.shawesh@gmail.com>
2023-01-12 11:12:02 +0300

    pex10

---
+++ chapter1/exercise/pex10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+
+    printf("BBB    Y    Y   EEEE\n");
+    printf("B   B   Y  Y    E\n");
+    printf("BBB       Y     EEEE\n");
+    printf("B   B     Y\n");
+
+
+    return 0;
+}
