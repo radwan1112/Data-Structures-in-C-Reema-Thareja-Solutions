c0e2696034138943a8427f9377235cf8c49f5480
Radwan AlShawesh <radwan.al.shawesh@gmail.com>
2023-01-12 11:42:31 +0300

    pex11

---
+++ chapter1/exercise/pex11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    printf("\n Decimal number = %d", number);
+    printf("\n Octal number = %o", number);
+    printf("\n Hexadecimal number = %x", number);
+
+    return 0;
+
+}
