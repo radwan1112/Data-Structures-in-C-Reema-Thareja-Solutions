72d0a120dc126861c0ab746b799453ee0f3f6cfd
Radwan AlShawesh <radwan.al.shawesh@gmail.com>
2023-01-25 12:44:23 +0300

    ex15

--- chapter3/ex15.c
+++ chapter3/ex15.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-int main(){
+int main() {
 
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int *ptr = arr;
     int *ptr2 = &arr[8];
 
-    while(ptr <= ptr2){
+    while(ptr <= ptr2) {
         printf("arr[%d] = %d \n", ptr, *ptr);
         ptr++;
     }
+
+
 
 }
