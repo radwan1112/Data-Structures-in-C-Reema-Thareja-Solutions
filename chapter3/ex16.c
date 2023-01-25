6c43ffefc90f9e3bee5f890ee2b06273366856c5
Radwan AlShawesh <radwan.al.shawesh@gmail.com>
2023-01-25 14:03:06 +0300

    ex16

--- chapter3/ex16.c
+++ chapter3/ex16.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
 
+int main(){
+
+    int arr[2][2] = {12, 34, 56, 32};
+    int i, j;
+
+    for(i = 0; i < 2; i++){
+        for(j = 0; j < 2; j++){
+            printf("arr[%d][%d] = %d\n", i, j, arr[i][j]);
+        }
+    }
+
+
+}
+
