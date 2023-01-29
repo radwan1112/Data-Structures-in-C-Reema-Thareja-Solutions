aa2613b27f4dc407f2bc4ce55f99200bf2f17f6d
Radwan AlShawesh <radwan.al.shawesh@gmail.com>
2023-01-29 11:57:56 +0300

    ex24

---
+++ chapter3/ex24.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+
+void fillArray(int *matrix, int rows, int cols)
+{
+
+    int i, j;
+
+    for(i = 0; i < rows; i++)
+    {
+        for(j = 0; j < cols; j++)
+        {
+            if(i == j)
+            {
+                matrix[i * cols + j] = 0;
+            }
+            else if(i < j)
+            {
+                matrix[i * cols + j] = -1;
+            }
+            else
+            {
+                matrix[i * cols + j] = 1;
+            }
+        }
+    }
+}
+
+void print_matrix(int *matrix, int rows, int cols)
+{
+
+    int i, j;
+
+    for(i = 0; i < rows; i++)
+    {
+        printf("| ");
+        for(j = 0; j < cols; j++)
+        {
+            printf(" %3d ", matrix[i * cols + j]);
+        }
+        printf(" |\n");
+    }
+
+
+}
+
+
+int main()
+{
+
+    printf("Generating a Square matrix...\n\n");
+    int rows, cols;
+    printf("Enter number of rows and columns: ");
+    scanf(" %d", &rows);
+    cols = rows;
+
+    int matrix[rows][cols];
+    fillArray(matrix, rows, cols);
+
+    print_matrix(matrix, rows, cols);
+
+
+
+    return 0;
+}
