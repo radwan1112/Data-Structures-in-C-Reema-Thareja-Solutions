60af9ba2e091af9cc933da92e577bc7296bab225
Radwan AlShawesh <radwan.al.shawesh@gmail.com>
2023-01-31 17:47:33 +0300

    pex6

---
+++ chapter3/exercises/pex6.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+void read(int *arr, int size)
+{
+
+    int i, j;
+    for(i = 0; i < size; i++)
+    {
+        for(j = 0; j < size; j++)
+        {
+            printf("arr[%d][%d] = ", i, j);
+            scanf(" %d", &arr[i * size + j]);
+        }
+    }
+}
+
+void display(int *arr, int size)
+{
+    int i, j;
+    for(i = 0; i < size; i++)
+    {
+        for(j = 0; j < size; j++)
+        {
+            printf("%d ", arr[i * size + j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int size;
+    printf("Enter the size of the square: ");
+    scanf("%d", &size);
+    int square[size][size];
+
+    read(square, size);
+    display(square, size);
+
+    return 0;
+}
