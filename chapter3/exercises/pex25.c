4833a75af8c101723078ecd2823431482ead64f7
Radwan AlShawesh <radwan.al.shawesh@gmail.com>
2023-02-07 11:06:50 +0300

    update

---
+++ chapter3/exercises/pex25.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+void auto_fill(float arr[], int len)
+{
+
+    int i;
+    for(i = 0; i < len; i++)
+    {
+        arr[i] = (float)rand() / RAND_MAX * 100;
+    }
+}
+
+void fill_array(float arr[], int len)
+{
+    int i;
+    for(i = 0; i < len; i++)
+    {
+        printf("Array[%2.5f]: ", i);
+        scanf(" %f", &arr[i]);
+    }
+}
+
+void print_array(float arr[], int len)
+{
+    int i;
+
+    printf("   {");
+    for(i = 0; i < len; i++)
+    {
+        if(i == len - 1)
+        {
+            printf("%2.5f}\n", arr[i]);
+        }
+        else
+        {
+            printf("%2.5f, ", arr[i]);
+        }
+    }
+}
+
+float *insert_element(float *arr, int len)
+{
+    int pos;
+    float new_element;
+    int new_len = len + 1;
+
+    printf("Enter the new element you wish to insert: ");
+    scanf(" %f", &new_element);
+
+    printf("Choose an index between (0 ~ %d)", new_len - 1);
+    int ret = scanf(" %d", &pos);
+    while(ret == 0 || pos < 0 || pos > new_len - 1)
+    {
+        printf("Choose an index between (0 ~ %d)", new_len - 1);
+        scanf(" %d", &pos);
+    }
+
+    float *new_array = (float*)malloc(new_len * sizeof(float));
+
+    int i, j;
+    for(i = )
+
+
+    }
+
+int main()
+{
+
+    srand(time(NULL));
+    printf("Enter the Length of the array: ");
+    int len;
+    scanf(" %d", &len);
+    float *arr = (float*)malloc(len * sizeof(float));
+
+    int answer;
+    printf("\n--Choose how to fill the matrix--\n");
+    printf("\n1. Automatically Generate Matrix\n");
+    printf("2. Manually Enter Matrix Elements\n\n");
+    printf("Enter: ");
+    scanf(" %d", &answer);
+
+    if(answer == 1)
+    {
+        auto_fill(arr, len);
+    }
+    else
+    {
+        fill_array(arr, len);
+    }
+    printf("\nPrinting array... \n\n");
+    print_array(arr, len);
+
+    int pos;
+    int number;
+    char reply;
+    do
+    {
+        printf("Do you want to insert a new Element? Type 'Y' or 'N' ");
+        scanf(" %c", &reply);
+        if(reply == 'y' || reply == 'Y')
+        {
+            insert_element(arr, len);
+        }
+        else
+        {
+            printf("Here is your final array Have a good day");
+            print_array();
+        }
+    }
+    while(reply == 'y' || reply == 'Y')
+
+
+
+        return 0;
+}
