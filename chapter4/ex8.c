28518b837bcbd8c483d35e96fa5dc831ffec1072
radwan <radwan.al.shawesh@gmail.com>
2023-02-12 22:32:24 +0300

    ex8

---
+++ chapter4/ex8.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("---Delete substring from a text---\n\n");
+    printf("Enter some text: ");
+    char text[100];
+    gets(text);
+
+    printf("Enter the substring you wish to delete: ");
+    char str[100];
+    gets(str);
+
+    int i = 0, j = 0, k = 0, n = 0;
+    char new_text[100];
+    int copy = 0;
+
+    while(text[i] != NULL)
+    {
+        j = i;
+        k = 0;
+        while(text[j] == str[k] && str[k] != '\0')
+        {
+            j++;
+            k++;
+        }
+
+        if(str[k] == '\0')
+        {
+            copy = j;
+        }
+
+        new_text[n] = text[copy];
+        i++;
+        n++;
+        copy++;
+
+    }
+    new_text[j] = NULL;
+
+    printf("Modifying text...\n\n");
+    printf(" %s", new_text);
+
+    return 0;
+}
