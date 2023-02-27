6efe2576c9bc492231d7d9f3b8645c5fbfa1f1da
radwan <radwan.al.shawesh@gmail.com>
2023-02-27 08:34:49 +0300

    pex7

---
+++ chapter5/exercise/pex7.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+struct department
+{
+
+    char name[100];
+    int age;
+    int year_of_xp;
+    char position[100];
+
+} employees[5];
+
+int main()
+{
+    for(int i = 0; i < 5; i++)
+    {
+        printf("\nEmployee no %d info\n\n", i+1);
+        printf("Enter name: ");
+        gets(employees[i].name);
+        printf("Enter the age: ");
+        scanf(" %d", &employees[i].age);
+        printf("Enter years of experience: ");
+        scanf(" %d", &employees[i].year_of_xp);
+        printf("Enter position: ");
+        getchar();
+        gets(employees[i].position);
+    }
+
+    for(int i = 0; i < 5; i++)
+    {
+        printf("\nEmployee no %d info\n\n", i+1);
+        printf("Name: %s", employees[i].name);
+        printf("Age: %d", &employees[i].age);
+        printf("Years of xp: %d", &employees[i].year_of_xp);
+        printf("Position: %s", employees[i].position);
+    }
+
+    int edit;
+    printf("enter the number of employee you wish to edit his details: ");
+    scanf(" %d", &edit);
+    printf("\nEmployee no %d info\n\n", edit);
+    edit--;
+    printf("Enter name: ");
+    gets(employees[edit].name);
+    printf("Enter the age: ");
+    scanf(" %d", &employees[edit].age);
+    printf("Enter years of experience: ");
+    scanf(" %d", &employees[edit].year_of_xp);
+    printf("Enter position: ");
+    getchar();
+    gets(employees[edit].position);
+
+    for(int i = 0; i < 5; i++)
+    {
+        printf("\nEmployee no %d info\n\n", i+1);
+        printf("Name: %s", employees[i].name);
+        printf("Age: %d", &employees[i].age);
+        printf("Years of xp: %d", &employees[i].year_of_xp);
+        printf("Position: %s", employees[i].position);
+    }
+
+
+    return 0;
+}
