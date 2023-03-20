410ee27bb5085b4c0c66594241e1a585234cbb03
radwan <radwan.al.shawesh@gmail.com>
2023-03-20 13:16:15 +0300

    ex2

---
+++ chapter8/ex2.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct queue
+{
+    struct node *front;
+    struct node *rear;
+};
+
+struct queue *q;
+
+void create_queue(struct queue *q)
+{
+    q->front = NULL;
+    q->rear = NULL;
+}
+
+struct queue *enqueue(struct queue *q, int val)
+{
+    struct node *new_node;
+    new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->data = val;
+    new_node->next = NULL;
+    if(q->front == NULL)
+    {
+        q->front = new_node;
+        q->rear = new_node;
+    }
+    else
+    {
+        q->rear->next = new_node;
+        q->rear = new_node;
+    }
+    return q;
+}
+
+struct queue *dequeue(struct queue *q)
+{
+    struct node *ptr;
+    ptr = q->front;
+
+    if(q->front == NULL)
+    {
+        printf("\n QUEUE UNDERFLOW");
+    }
+    else
+    {
+        q->front = q->front->next;
+        free(ptr);
+    }
+    return q;
+};
+
+int peek(struct queue *q)
+{
+    if(q->front == NULL)
+    {
+        printf("\n QUEUE UNDERFLOW");
+        return -1;
+    }
+    else
+    {
+        return q->front->data;
+    }
+}
+
+void display(struct queue *q)
+{
+    if(q->front == NULL)
+    {
+        printf("\n QUEUE UNDERFLOW");
+        return;
+    }
+    struct node *ptr;
+    ptr = q->front;
+
+    printf("\n ");
+    while(ptr->next != NULL)
+    {
+        printf("%d -> ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("%d", ptr->data);
+}
+
+
+int main()
+{
+    int option, val;
+    q = (struct queue *)malloc(sizeof(struct queue));
+    create_queue(q);
+
+    do
+    {
+        printf("\n *****MAIN MENU*****");
+        printf("\n 1. ENQUEUE");
+        printf("\n 2. DEQUEUE");
+        printf("\n 3. PEEK");
+        printf("\n 4. DISPLAY");
+        printf("\n 5. EXIT\n");
+
+        printf("\n Enter your option : ");
+        scanf("%d", &option);
+
+        switch(option)
+        {
+        case 1:
+            printf("\n Enter the number to insert: ");
+            scanf(" %d", &val);
+            q = enqueue(q, val);
+            break;
+        case 2:
+            val = peek(q);
+            q = dequeue(q);
+            if(val != -1)
+            {
+                printf("\n The value dequeued is %d", val);
+            }
+            break;
+        case 3:
+            val = peek(q);
+            if(val != -1)
+            {
+                printf("\n The value at front is %d", val);
+            }
+            break;
+        case 4:
+            display(q);
+            break;
+        default:
+            option = 5;
+            break;
+        }
+    }
+    while(option != 5);
+
+    return 0;
+}
